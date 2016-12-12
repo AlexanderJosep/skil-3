f9c04a6d0c41d744a4e6e96f847fb59c3ec6b3af
kraftastrumpur <kristjan_bk@hotmail.com>
2016-12-12 13:44:17 +0000

    Breytingar á töfluni

--- Skil3/ui_mainwindow.h
+++ Skil3/ui_mainwindow.h
@@ -22,6 +22,7 @@
 #include <QtWidgets/QRadioButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTableView>
+#include <QtWidgets/QTableWidget>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
 
@@ -46,6 +47,7 @@ public:
     QLineEdit *lineSearch;
     QLineEdit *lineFilter;
     QTableView *tableViewPersons;
+    QTableWidget *tableWidget;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -54,7 +56,7 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(838, 326);
+        MainWindow->resize(937, 326);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         radioButtonP1 = new QRadioButton(centralWidget);
@@ -104,10 +106,53 @@ public:
         tableViewPersons = new QTableView(centralWidget);
         tableViewPersons->setObjectName(QStringLiteral("tableViewPersons"));
         tableViewPersons->setGeometry(QRect(120, 10, 231, 181));
+        tableViewPersons->setEditTriggers(QAbstractItemView::NoEditTriggers);
+        tableViewPersons->setSelectionBehavior(QAbstractItemView::SelectRows);
+        tableViewPersons->setSortingEnabled(true);
+        tableViewPersons->verticalHeader()->setVisible(false);
+        tableWidget = new QTableWidget(centralWidget);
+        if (tableWidget->columnCount() < 4)
+            tableWidget->setColumnCount(4);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(1, __qtablewidgetitem1);
+        QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(2, __qtablewidgetitem2);
+        QTableWidgetItem *__qtablewidgetitem3 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(3, __qtablewidgetitem3);
+        if (tableWidget->rowCount() < 2)
+            tableWidget->setRowCount(2);
+        QTableWidgetItem *__qtablewidgetitem4 = new QTableWidgetItem();
+        tableWidget->setVerticalHeaderItem(0, __qtablewidgetitem4);
+        QTableWidgetItem *__qtablewidgetitem5 = new QTableWidgetItem();
+        tableWidget->setVerticalHeaderItem(1, __qtablewidgetitem5);
+        QTableWidgetItem *__qtablewidgetitem6 = new QTableWidgetItem();
+        tableWidget->setItem(0, 0, __qtablewidgetitem6);
+        QTableWidgetItem *__qtablewidgetitem7 = new QTableWidgetItem();
+        tableWidget->setItem(0, 1, __qtablewidgetitem7);
+        QTableWidgetItem *__qtablewidgetitem8 = new QTableWidgetItem();
+        tableWidget->setItem(0, 2, __qtablewidgetitem8);
+        QTableWidgetItem *__qtablewidgetitem9 = new QTableWidgetItem();
+        tableWidget->setItem(0, 3, __qtablewidgetitem9);
+        QTableWidgetItem *__qtablewidgetitem10 = new QTableWidgetItem();
+        tableWidget->setItem(1, 0, __qtablewidgetitem10);
+        QTableWidgetItem *__qtablewidgetitem11 = new QTableWidgetItem();
+        tableWidget->setItem(1, 1, __qtablewidgetitem11);
+        QTableWidgetItem *__qtablewidgetitem12 = new QTableWidgetItem();
+        tableWidget->setItem(1, 2, __qtablewidgetitem12);
+        QTableWidgetItem *__qtablewidgetitem13 = new QTableWidgetItem();
+        tableWidget->setItem(1, 3, __qtablewidgetitem13);
+        tableWidget->setObjectName(QStringLiteral("tableWidget"));
+        tableWidget->setGeometry(QRect(410, 10, 511, 192));
+        tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+        tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+        tableWidget->setSortingEnabled(true);
+        tableWidget->verticalHeader()->setVisible(false);
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 838, 19));
+        menuBar->setGeometry(QRect(0, 0, 937, 26));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
         mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
@@ -136,6 +181,39 @@ public:
         viewPButton->setText(QApplication::translate("MainWindow", "View", 0));
         pushButton_5->setText(QApplication::translate("MainWindow", "Search", 0));
         pushButton_6->setText(QApplication::translate("MainWindow", "Filter", 0));
+        QTableWidgetItem *___qtablewidgetitem = tableWidget->horizontalHeaderItem(0);
+        ___qtablewidgetitem->setText(QApplication::translate("MainWindow", "Name", 0));
+        QTableWidgetItem *___qtablewidgetitem1 = tableWidget->horizontalHeaderItem(1);
+        ___qtablewidgetitem1->setText(QApplication::translate("MainWindow", "Birth Year", 0));
+        QTableWidgetItem *___qtablewidgetitem2 = tableWidget->horizontalHeaderItem(2);
+        ___qtablewidgetitem2->setText(QApplication::translate("MainWindow", "Gender", 0));
+        QTableWidgetItem *___qtablewidgetitem3 = tableWidget->horizontalHeaderItem(3);
+        ___qtablewidgetitem3->setText(QApplication::translate("MainWindow", "Death Year", 0));
+        QTableWidgetItem *___qtablewidgetitem4 = tableWidget->verticalHeaderItem(0);
+        ___qtablewidgetitem4->setText(QApplication::translate("MainWindow", "New Row", 0));
+        QTableWidgetItem *___qtablewidgetitem5 = tableWidget->verticalHeaderItem(1);
+        ___qtablewidgetitem5->setText(QApplication::translate("MainWindow", "New Row", 0));
+
+        const bool __sortingEnabled = tableWidget->isSortingEnabled();
+        tableWidget->setSortingEnabled(false);
+        QTableWidgetItem *___qtablewidgetitem6 = tableWidget->item(0, 0);
+        ___qtablewidgetitem6->setText(QApplication::translate("MainWindow", "Kristjan", 0));
+        QTableWidgetItem *___qtablewidgetitem7 = tableWidget->item(0, 1);
+        ___qtablewidgetitem7->setText(QApplication::translate("MainWindow", "1993", 0));
+        QTableWidgetItem *___qtablewidgetitem8 = tableWidget->item(0, 2);
+        ___qtablewidgetitem8->setText(QApplication::translate("MainWindow", "KK", 0));
+        QTableWidgetItem *___qtablewidgetitem9 = tableWidget->item(0, 3);
+        ___qtablewidgetitem9->setText(QApplication::translate("MainWindow", "NOPE", 0));
+        QTableWidgetItem *___qtablewidgetitem10 = tableWidget->item(1, 0);
+        ___qtablewidgetitem10->setText(QApplication::translate("MainWindow", "Lubbi", 0));
+        QTableWidgetItem *___qtablewidgetitem11 = tableWidget->item(1, 1);
+        ___qtablewidgetitem11->setText(QApplication::translate("MainWindow", "2003", 0));
+        QTableWidgetItem *___qtablewidgetitem12 = tableWidget->item(1, 2);
+        ___qtablewidgetitem12->setText(QApplication::translate("MainWindow", "kk", 0));
+        QTableWidgetItem *___qtablewidgetitem13 = tableWidget->item(1, 3);
+        ___qtablewidgetitem13->setText(QApplication::translate("MainWindow", "nope", 0));
+        tableWidget->setSortingEnabled(__sortingEnabled);
+
     } // retranslateUi
 
 };
